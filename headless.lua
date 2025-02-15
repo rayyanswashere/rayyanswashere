5fb646bfec9e7bd81643cd4fb7020eaeaeccdf23
rayyanswashere <memes4fun45@gmail.com>
2025-02-14 18:17:28 -0600

    Create headless.lua

---
+++ headless.lua
@@ -0,0 +1,12 @@
+local player = game.Players.LocalPlayer
+if player and player.Character then
+    local head = player.Character:FindFirstChild("Head")
+    if head then
+        head.Transparency = 1
+        local face = head:FindFirstChildOfClass("Decal")
+        if face then
+            face.Transparency = 1
+        end
+    end
+end
+there
